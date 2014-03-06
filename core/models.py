ac9db00aac77d7861bb6491628746dece0bbbe43
Riley Thomasson <riley.thomasson@gmail.com>
2014-03-06 09:05:13 -0800

    Added HistoryNode model

---
+++ core/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class HistoryNode(models.Model):
+  # choices for the transition type field
+  LINK = 0
+  TYPED = 1
+  AUTO_BOOKMARK = 2
+  AUTO_SUBFRAME = 3
+  MANUAL_SUBFRAME = 4
+  GENERATED = 5
+  AUTO_TOPLEVEL = 6
+  FORM_SUBMIT = 7
+  RELOAD = 8
+  KEYWORD = 9
+  KEYWORD_GENERATED = 10
+  TRANSITION_CHOICES = (
+    (LINK, 'link'),
+    (TYPED, 'typed'),
+    (AUTO_BOOKMARK, 'auto_bookmark'),
+    (AUTO_SUBFRAME, 'auto_subframe'),
+    (MANUAL_SUBFRAME, 'manual_subframe'),
+    (GENERATED, 'generated'),
+    (AUTO_TOPLEVEL, 'auto_toplevel'),
+    (FORM_SUBMIT, 'form_submit'),
+    (RELOAD, 'reload'),
+    (KEYWORD, 'keyword'),
+    (KEYWORD_GENERATED, 'keyword_generated'),
+  )
+
+  # fields
+  url = models.URLField(max_length=2048)
+  last_title = models.CharField(max_length=256)
+  visit_time = models.DateTimeField()
+  transition_type = models.IntegerField(choices=TRANSITION_CHOICES)
+  browser_id = models.IntegerField()
+  referrer = models.ForeignKey('HistoryNode', blank=True, null=True)
