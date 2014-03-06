8db06982570fc5575d35051b7d88bd6b9978dd6b
Ankush Gola <ankush@nat-tvwna-inside-visitornet101-b-31249.princeton.org>
2014-03-06 15:29:31 -0500

    changes to views.py

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+from django.core import serializers
+import json
+
+
+def respond(request):
+    now = datetime.datetime.now()
+    return HttpResponse(html)
 
 # Create your views here.
