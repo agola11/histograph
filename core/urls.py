570148dc81c9d9c20587d80aa7559f875cf61506
Riley Thomasson <riley.thomasson@gmail.com>
2014-03-06 13:34:49 -0800

    fixed url mappings in core

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, url
 from core import views
 
 urlpatterns = patterns('',
-    url(r'^store', views.store_history, name='store_history')
-    url(r'^send', views.store_history, name='send_history')
-
+    url(r'^store$', views.store_history, name='store_history'),
+    url(r'^send$', views.send_history, name='send_history')
 )
