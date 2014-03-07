6074c970d86012304c8e7f50984756cbf3bbdd7d
Aaron <ahimelman@gmail.com>
2014-03-07 03:09:09 -0500

    basic front-end functionality. without all static files committed (i.e. no bootstrap, angular, etc.). we want that in one location on the site)

---
+++ histograph/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for histograph project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os, sys
+
+sys.path.append('/var/www/histograph')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "histograph.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
