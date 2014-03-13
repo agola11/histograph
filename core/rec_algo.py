32b1eb09aaa50ddaa793437939ae3729c1d21180
Ankush Gola <ankush@nat-tvwna-inside-visitornet101-b-31249.princeton.org>
2014-03-13 04:58:34 -0400

    added features to rec_algo, still lots to do

--- core/rec_algo.py
+++ core/rec_algo.py
@@ -1,8 +1,25 @@
-#TODO: this file contains code for recommendation algorithm.
+from core.models import HistoryNode
+from urlparse import urlparse
+from django.utils import simpleson
 import numpy
 
-def get_frequencies():
-	pass
+def filter_http(hn):
+	l = hn['url'].split(':')
+	return(l[0] == 'http')
 
-def rank_users():
+def clean_url(hn):
+	url = hn['url']
+	if url.startswith('http://'):
+		hn['url'] = url[7:]
+	return hn
+
+# TODO: change extension_id to user_id once user auth is implemented
+# TODO: use values (not objects.all()) to rid unused values
+def get_frequencies(depth):
+	hn_list = list(HistoryNode.objects.all())
+	hn_list = map(clean_url, filter(filter_http, hn_list))
+	hn_list = sorted(hn_list, key=lambda hn: hn['url'])
+
+
+def rank_users(user):
 	pass
