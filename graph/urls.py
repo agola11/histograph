1072b772c6d6b3048a8b2e5cbe00d69eac851791
Ankush Gola <ankush@nat-tvwna-inside-visitornet101-b-31249.princeton.org>
2014-03-30 22:32:43 -0400

    bubble request

--- graph/urls.py
+++ graph/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 from graph import views
 
 urlpatterns = patterns('',
-    url(r'^bubble$', views.send_bubble, name='bubble'),
+    url(r'^bubble/(?P<user_id>\d+)$', views.send_user_bubble, name='bubble'),
 )
--- graph/views.py
+++ graph/views.py
@@ -4,49 +4,10 @@ from django.core import serializers
 from django.http import HttpResponse
 from string import split
 from django.utils import simplejson
+from django.http import Http404
+import graph_utils
 
-def send_bubble(request):
-  resp = HttpResponse()
-  all_nodes = HistoryNode.objects.all()
-  
-  bubble_root = {}
-  bubble_root['name'] = 'top_level'
-  bubble_root['node_count'] = 0
-  bubble_root['children'] = {}
-  
-  for node in all_nodes:
-    urls = split(node.url, '://', 1)
-    if len(urls) == 1:
-      url = urls[0]
-    else:
-      url = urls[1]
-    
-    bubble_node = bubble_root
+def send_user_bubble(request, user_id):
+	bubble_tree = graph_utils.send_user_bubble(user)
+	return HttpResponse(simplejson.dumps(bubble_tree), content_type='application/json')
 
-    bubble_root['node_count'] = bubble_root['node_count'] + 1
-
-    while True:
-      if '/' in url:
-        url_split = split(url, '/', 1)
-        name = url_split[0]
-      else:
-        name = url
-
-      # initialize node if empty
-      if not name in bubble_node['children']:
-        bubble_node['children'][name] = {}
-        bubble_node = bubble_node['children'][name]
-        bubble_node['name'] = name
-        bubble_node['node_count'] = 0
-        bubble_node['children'] = {}
-      else:
-        bubble_node = bubble_node['children'][name]
-
-      bubble_node['node_count'] = bubble_node['node_count'] + 1
-
-      if not '/' in url:
-        break
-
-      url = url_split[1]
-
-  return HttpResponse(simplejson.dumps(bubble_root), content_type='application/json')
