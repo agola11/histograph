8a93b484790e29d1d5af9c2698d79d0f7142437b
akshayau <akshaya.uttamadoss@gmail.com>
2014-04-01 23:08:32 -0400

    sequence sunburst

--- graph/urls.py
+++ graph/urls.py
@@ -4,5 +4,6 @@ from graph import views
 urlpatterns = patterns('', 
     url(r'^bubble/(?P<user_id>\d+)$', views.send_user_bubble, name='user_bubble'),
     url(r'^bubble$', views.send_bubble, name='bubble'), 
-    url(r'^pie$', views.pie, name='pie'), 
+    url(r'^pie$', views.pie, name='pie'),
+    url(r'^sunburst$', views.pie, name='sunburst'), 
 )
