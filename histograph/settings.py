02583b70992081c8580e46ff03af5b2a4f5a6837
Riley Thomasson <riley.thomasson@gmail.com>
2014-04-05 13:53:02 -0700

    added atomic transaction when saving history and added logging to test performance

--- histograph/settings.py
+++ histograph/settings.py
@@ -96,6 +96,25 @@ STATICFILES_FINDERS = (
 
 AUTH_USER_MODEL = 'django_facebook.FacebookCustomUser'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'debug.log'),
+        },
+    },
+    'loggers': {
+        'core.models': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
