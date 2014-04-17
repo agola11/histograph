6e7e8f1a21f677732dcb23837243a0082d0a1b1a
Riley Thomasson <riley.thomasson@gmail.com>
2014-04-16 20:34:52 -0700

    added basic date filtering on sunburst

--- static/sidebar.js
+++ static/sidebar.js
@@ -19,59 +19,59 @@
             // Se agregan elementos básicos
             // $('<div/>', { id: 'sliding_menu_js_btn'}).appendTo('body');
 
-            $('<div/>', { id: 'sliding_menu_js', class: 'cerrado' }).appendTo('body');
+            // $('<div/>', { id: 'sliding_menu_js', class: 'cerrado' }).appendTo('body');
 
-            $('<div/>', { class: 'header' }).appendTo('#sliding_menu_js');
+            // $('<div/>', { class: 'header' }).appendTo('#sliding_menu_js');
 
-            $('<ul/>').appendTo('#sliding_menu_js');
+            // $('<ul/>').appendTo('#sliding_menu_js');
 
-            $('<div/>', { id: 'sliding_menu_js_over' }).appendTo('body');
+            // $('<div/>', { id: 'sliding_menu_js_over' }).appendTo('body');
 
 
             // Se agrega un padding top para mostrar todo el contenido del sitio
             // $('body').css('padding-top', '60px');
 
             // Se copia el menu original
-            $('#sliding_menu_js ul').append($(this).html());
+            // $('#leftmenu ul').append($(this).html());
 
             // Se eliminan elementos innecesarios
-            $('.divider').remove();
-            $('#sliding_menu_js ul').removeClass();
-            $('#sliding_menu_js ul li').removeClass();
-            $('#sliding_menu_js ul li a').removeClass();
+            // $('.divider').remove();
+            // $('#leftmenu ul').removeClass();
+            // $('#leftmenu ul li').removeClass();
+            // $('#leftmenu ul li a').removeClass();
 
             // Titulo
-            if (config.header_title) {
-                $('#sliding_menu_js .header').prepend("<h3>" + config.header_title + "</h3>");
-                $('#sliding_menu_js_btn').append("<h3>" + config.header_title + "</h3>");
-            }
+            // if (config.header_title) {
+            //     $('#leftmenu .header').prepend("<h3>" + config.header_title + "</h3>");
+            //     // $('#sliding_menu_js_btn').append("<h3>" + config.header_title + "</h3>");
+            // }
 
             // Logo
-            if (config.header_logo) {
-                $('#sliding_menu_js .header').prepend("<img src='" + config.header_logo + "' />");
-            }
+            // if (config.header_logo) {
+            //     $('#leftmenu .header').prepend("<img src='" + config.header_logo + "' />");
+            // }
 
             // Transición
-            $('#sliding_menu_js').css('transition', 'left ' + config.transitionSpeed + 's ' + config.easing);
+            $('#leftmenu').css('transition', 'left ' + config.transitionSpeed + 's ' + config.easing);
 
 
             $('#leftButton').click(function () {
                 toggle();
             });
 
-            $('#sliding_menu_js_over').click(function () {
+            $('#leftmenu_over').click(function () {
                 ocultar();
             });
 
             // Al presionar cualquier enlace dentro del menu
-            $('#sliding_menu_js ul li a').click(function () {
+            $('#leftmenu ul li a').click(function () {
                 // ocultar();
             });
 
 
             // Muestra/Oculta el panel
             function toggle (){
-                if ( $('#sliding_menu_js').hasClass('open') ) {
+                if ( $('#leftmenu').hasClass('open') ) {
                     ocultar();
                 } else {
                     mostrar();
@@ -80,21 +80,21 @@
 
             // Muestra la barra lateral
             function mostrar(){
-                if ( $('#sliding_menu_js').hasClass('cerrado') ) {
-                    $('#sliding_menu_js').css('left','0px');
-                    $('#sliding_menu_js').removeClass('cerrado');
-                    $('#sliding_menu_js').addClass('open');
-                    $('#sliding_menu_js_over').show();
+                if ( $('#leftmenu').hasClass('cerrado') ) {
+                    $('#leftmenu').css('left','0px');
+                    $('#leftmenu').removeClass('cerrado');
+                    $('#leftmenu').addClass('open');
+                    $('#leftmenu_over').show();
                 };
             }
 
             // Oculta la barra lateral
             function ocultar(){
-                if ( $('#sliding_menu_js').hasClass('open') ) {
-                    $('#sliding_menu_js').css('left','-300px');
-                    $('#sliding_menu_js').removeClass('open')
-                    $('#sliding_menu_js').addClass('cerrado')
-                    $('#sliding_menu_js_over').hide();
+                if ( $('#leftmenu').hasClass('open') ) {
+                    $('#leftmenu').css('left','-300px');
+                    $('#leftmenu').removeClass('open')
+                    $('#leftmenu').addClass('cerrado')
+                    $('#leftmenu_over').hide();
                 };
             }
         },
