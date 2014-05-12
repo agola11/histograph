52ef1ff03d591fc326206d576e10212809bef9a7
Akshaya Uttamadoss <akshaya@meinkraft.linkpc.net>
2014-05-12 14:13:27 -0700

    clean up views and urls for graph and fix bugs in html

--- graph/urls.py
+++ graph/urls.py
@@ -2,20 +2,14 @@ from django.conf.urls import patterns, url
 from graph import views
 
 urlpatterns = patterns('', 
-    #url(r'^bubble/(?P<user_id>\d+)$', views.send_user_bubble, name='user_bubble'),
     url(r'^bubble/(?P<timesetting>\w+)$', views.send_bubble, name='bubble'), 
-    url(r'^bubble_u/(?P<user_id>\d+)$', views.send_bubble_u, name='bubble_u'),
     url(r'^bubble_blocked$', views.send_bubble_blocked, name='bubble_blocked'), 
-    url(r'^pie$', views.pie, name='pie'),
-    url(r'^line/(?P<user_id>\d+)$', views.send_user_line_plot, name='user_line'),
     url(r'^line$', views.send_line_plot, name='user_line'),
     url(r'^lineplot$', views.line_plot, name='line_plot'),
-    url(r'^sunburst/(?P<user_id>\d+)$', views.user_sunburst, name='user_sunburst'),
     url(r'^sunburst$', views.sunburst, name='sunburst'),
     url(r'^circle$', views.circle, name='circle'), 
     url(r'^digraphdata/(?P<timesetting>\w+)$', views.send_digraph, name='send_digraph'),
     url(r'^digraph$', views.digraph, name='digraph'),
     url(r'^friends$', views.friends, name='friends'),
     url(r'^friendsdata$', views.send_friends, name='send_friends'),
-    url(r'^area$', views.area, name='area'),
 )
